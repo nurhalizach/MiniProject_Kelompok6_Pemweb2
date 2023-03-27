7fe90efe95fbaf7c11976a0607aeeb7be4439884
Delphia Aryana <112560412+DelphiaAryana@users.noreply.github.com>
2023-03-27 17:48:47 +0800

    Add files via upload
    halaman home admin

---
+++ MiniProject_Kelompok6/app/views/admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+    </head>
+    <body>
+	<div class = "container-fluid">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<center><img src = "<?=BASEURL?>/img/back.jpg" width="800px" height="600px" /></center>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+</body>
+<script src = "../js/jquery.js"></script>
+<script src = "../js/bootstrap.js"></script>	
+</html>
